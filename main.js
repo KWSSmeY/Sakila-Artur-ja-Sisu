86763c5479a7bf9aae3b93e6e4a31c3ccf56ce2a
Sisu Salakoski <s2200896@edu.bc.fi>
2024-04-26 12:18:15 +0300

    Elokuvatietojen näyttäminen etusivulla

--- main.js
+++ main.js
@@ -5,50 +5,44 @@ const path = require("path");
 const { port, host } = require("./configs/config.json");
 const dbconfig = require("./configs/dbconfig.json");
 
-
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'sivupohjat'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "sivupohjat"));
 
-
-
-function getInfo(res, query1, query2) {
+function getInfo(res, query) {
   const connection = mysql.createConnection(dbconfig);
   connection.connect();
-  connection.query(query1, (err, rows1) => {
+  connection.query(query, (err, rows) => {
     if (err) {
       throw err;
     }
 
-    connection.query(query2, (err, rows2) => {
-      if (err) {
-        throw err;
-      }
+    const shuffledFilms = rows.sort(() => Math.random() - 0.5);
+    const selectedFilms = shuffledFilms.slice(0, 6);
 
-      // Send data as JSON
-      res.json({ films: rows1, categories: rows2 });
-    });
+    const films = selectedFilms.map((row) => ({
+      title: row.title,
+      length: row.length,
+      description: row.description,
+    }));
+
+    res.json({ elokuvat: films });
   });
 }
+
+// Provide data endpoint
+app.get("/data", (req, res) => {
+  getInfo(res, "SELECT title, length, description FROM film");
+});
 
 // Serve static files from the 'styles' directory
 app.use("/styles", express.static(path.join(__dirname, "styles")));
 
 app.use("/photos", express.static(path.join(__dirname, "photos")));
 
-
-// Provide data endpoint
-app.get("/data", (req, res) => {
-  getInfo(
-    res,
-    "SELECT title FROM film",
-    "SELECT * FROM category"
-  );
-});
-
-app.get("/", (req, res) =>{
-  res.render('etusivu', {});
+app.get("/", (req, res) => {
+  res.render("etusivu", {});
 });
 
 app.listen(port, host, () => {
